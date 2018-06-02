7ba509acd2cb2ef8475fbccb234f8ee4ad48a5c4
Jann <jbenjamin001@gmail.com>
2018-06-02 13:55:17 +0530

    v1

---
+++ chat.py
@@ -0,0 +1,42 @@
+from elizachat import eliza_chat #import elizachat
+from rudechat import rude_chat #import rudechat
+from warchat import suntsu_chat #import warchat
+from zenchat import zen_chat #import zenchat
+from casualchat import iesha_chat #import casualchat
+
+import random
+import re
+
+print '='*72
+print "\n"
+print "Talk to the program by typing in plain English, using normal upper-"
+print 'and lower-case letters and punctuation.  Enter "quit" when done.'
+print '='*72
+print "Hello.  How are you feeling today?"
+
+
+def main():
+
+  while True:
+        statement = raw_input("> ")
+        x = random.randint(1,5)
+        if x == 1 :
+            print eliza_chat(statement)
+
+        elif x == 2:
+            print rude_chat(statement)
+
+        elif x == 3 :
+            print suntsu_chat(statement)
+
+        elif x == 4:
+            print iesha_chat(statement)
+
+        else:
+            print zen_chat(statement)
+
+        if statement == "quit":
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+name = 'chatbot',
+version = '1.0.0',
+py_modules = ['chat','rudechat','casualchat','elizachat','warchat','zenchat'],
+author = 'thatsparrow',
+author_email = 'thatsparrow@protonmail.ch',
+url = 'https://thatsparrow.github.io',
+description = 'A chatbot using NLP',
+)
